2898932430a05791c10eca31e3ebe193b7b23778
girishdev <girishkumar.aorg@gmail.com>
2018-11-03 10:50:12 +0530

    first commit on VueJs

---
+++ Notes.php
@@ -0,0 +1,18 @@
+DOM Events:
+    click 
+    mouse enter 
+    mouse hover 
+    etc...
+
+Known Options for Vue Instance:
+    el: Connect to DOM 
+    data: Store Data to be used 
+    methods: Methods of this Vue Instance
+    computed: Dependent Properties 
+    watch: Execute code upon data changes
+
+Short code
+    v-on:click = @click 
+    v-bind:href = :href
+
+    
